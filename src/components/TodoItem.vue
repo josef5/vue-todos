0488e4db6a970cb1fc0aacbc0dd4543a3c969ed6
Jose Espejo <joseespejo@yahoo.com>
2024-09-14 15:46:54 +0100

    feat: Add TodoItem and TodoList components

---
+++ src/components/TodoItem.vue
@@ -0,0 +1,46 @@
+<script setup lang="ts">
+import { Button } from '@/components/ui/button'
+import { Checkbox } from '@/components/ui/checkbox'
+import { DeleteIcon, EditIcon } from '@/components/ui/icons'
+import { Label } from '@/components/ui/label'
+import { useTodoStore } from '@/stores/todoStore'
+import type { Todo } from '@/types/todo'
+import { ref, watch } from 'vue'
+
+const todoStore = useTodoStore()
+
+const props = defineProps<{ todo: Todo }>()
+const emit = defineEmits(['edit-todo'])
+const checkboxDone = ref(props.todo.done)
+
+watch(checkboxDone, (newValue) => {
+  todoStore.updateTodo({ ...props.todo, done: newValue })
+})
+</script>
+
+<template>
+  <div class="my-2 flex items-center justify-between gap-4 rounded-lg border px-3 py-2">
+    <Checkbox v-model:checked="checkboxDone" />
+    <Label
+      :style="{ textDecoration: checkboxDone ? 'line-through' : 'none' }"
+      class="flex-grow leading-snug"
+      >{{ todo.text }}</Label
+    >
+    <Button
+      size="icon"
+      variant="outline"
+      class="h-6 w-6 shrink-0"
+      @click="emit('edit-todo', props.todo)"
+      ><EditIcon
+    /></Button>
+    <Button
+      size="icon"
+      variant="outline"
+      class="h-6 w-6 shrink-0 hover:text-red-500"
+      @click="todoStore.deleteTodo(props.todo)"
+      ><DeleteIcon
+    /></Button>
+  </div>
+</template>
+
+<style scoped></style>
---
+++ src/components/TodoList.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+import TodoItem from '@/components/TodoItem.vue'
+import { Button } from '@/components/ui/button'
+import { useTodoStore } from '@/stores/todoStore'
+
+const todoStore = useTodoStore()
+
+const emit = defineEmits(['add-todo', 'edit-todo'])
+</script>
+
+<template>
+  <div class="">
+    <ul class="my-8">
+      <li v-for="todo in todoStore.todos" :key="todo.id" class="">
+        <TodoItem :todo="todo" @edit-todo="emit('edit-todo', todo)" />
+      </li>
+    </ul>
+    <div class="flex justify-end gap-2">
+      <Button
+        variant="outline"
+        size="sm"
+        v-if="todoStore.completedTodosCount > 0"
+        @click="todoStore.clearCompletedTodos"
+        >Clear Completed</Button
+      ><Button variant="outline" size="sm" @click="emit('add-todo')">Add Todo</Button>
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
